c3f7eaef7e34752b4a88542563b26f057a949e54
vaibhav kalne <vaibhavkalne143@gmail.com>
2018-10-21 10:42:52 +0530

    added

---
+++ 01_TS_Basics/01_app.js
@@ -0,0 +1,10 @@
+// Hello World App
+var greetMsg = "Hello Typescript";
+console.log(greetMsg);
+// Variables Creation in TypeScript
+function sum(a, b) {
+    var add = a + b;
+    return add;
+}
+var result = sum(20, 20);
+console.log("sum of a & b is : " + result);
---
+++ 01_TS_Basics/01_app.ts
@@ -0,0 +1,12 @@
+// Hello World App
+let greetMsg:string = "Hello Typescript";
+console.log(greetMsg);
+
+// Variables Creation in TypeScript
+function sum(a:number,b:number):number{
+    let add:number = a + b;
+    return add;
+
+}
+let result:number = sum(20,20)
+console.log("sum of a & b is : " + result);
